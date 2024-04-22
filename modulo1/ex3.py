38f5c005e84a6d7f8083d2e38d6ea7ed5bec4f1d
gongas123lol <114987902+gongas123lol@users.noreply.github.com>
2024-04-22 17:49:58 +0100

    Update ex3.py

--- modulo1/ex3.py
+++ modulo1/ex3.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
-
+#3-A-
 class pair:
     def __init__(self, word, cnt):
         self.word = word
@@ -78,4 +78,79 @@ def main():
 
 
 main()
-#print_histogram_and_stuff("alice29.txt")
+#..........................................................
+#3-b-
+
+def calculate_symbol_frequency(text):
+    text = text.replace('\n', '')
+    frequencies = {}
+    total_characters = 0
+
+    for char in text:
+        if char in frequencies:
+            frequencies[char] += 1
+        else:
+            frequencies[char] = 1
+        total_characters += 1
+
+    for char, occurrences in frequencies.items():
+        frequencies[char] = (occurrences / total_characters) * 100
+
+    return frequencies
+
+
+def calculate_pair_frequency(text):
+    text = text.replace('\n', '')
+    pair_frequencies = {}
+    total_pairs = 0
+
+    for i in range(len(text) - 1):
+        pair = text[i:i + 2]
+        if pair in pair_frequencies:
+            pair_frequencies[pair] += 1
+        else:
+            pair_frequencies[pair] = 1
+        total_pairs += 1
+
+    for pair, occurrences in pair_frequencies.items():
+        pair_frequencies[pair] = (occurrences / total_pairs) * 100
+
+    return pair_frequencies
+
+
+def analyze_file(file_path):
+    codecs = ['utf-8', 'iso-8859-1']
+    for codec in codecs:
+        try:
+            with open(file_path, 'r', encoding=codec) as file:
+                text = file.read()
+                symbol_frequency = calculate_symbol_frequency(text)
+                pair_frequency = calculate_pair_frequency(text)
+                return symbol_frequency, pair_frequency
+        except UnicodeDecodeError:
+            continue
+
+def print_top5(dictionary):
+    sorted_items = sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
+    return sorted_items[:5]
+
+
+# Input files
+english_file = 'ListaPalavrasEN.txt'
+portuguese_file = 'ListaPalavrasPT.txt'
+
+
+print("ListaPalavrasEN.txt")
+symbol_frequency_english, pair_frequency_english = analyze_file(english_file)
+print("5 most frequent symbols:")
+print(print_top5(symbol_frequency_english))
+print("5 most frequent symbol pairs:")
+print(print_top5(pair_frequency_english))
+
+print("ListaPalavrasPT.txt")
+symbol_frequency_portuguese, pair_frequency_portuguese = analyze_file(portuguese_file)
+print("5 most frequent symbols:")
+print(print_top5(symbol_frequency_portuguese))
+print("5 most frequent symbol pairs:")
+print(print_top5(pair_frequency_portuguese))
+

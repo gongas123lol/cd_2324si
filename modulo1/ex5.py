e0d09df64513cd229e19109530e5392211e75629
gongas123lol <114987902+gongas123lol@users.noreply.github.com>
2024-04-22 18:09:40 +0100

    Update ex5.py

--- modulo1/ex5.py
+++ modulo1/ex5.py
@@ -1,38 +1,99 @@
-import numpy as np
 import cv2
+import numpy as np
 
+
+# generate Key
 def generate_key(image_shape):
-    key = np.random.randint(0, 256, size=image_shape, dtype=np.uint8)
-    return key
+    key = np.random.randint(0, 256, image_shape)
+    return key.astype(np.uint8)
 
-def encrypt(image, key, start_x=None, start_y=None, end_x=None, end_y=None):
-    cropped_area = image[start_y:end_y, start_x:end_x]
-    cropped_key = generate_key(cropped_area.shape)
-    encrypted_area = np.zeros_like(cropped_area)
-    for i in range(3):
-        encrypted_area[:,:,i] = cv2.bitwise_xor(cropped_area[:,:,i], cropped_key[:,:,i])
-    image[start_y:end_y, start_x:end_x] = encrypted_area
-    return image
 
-def decrypt(encrypted_image, key, start_x=None, start_y=None, end_x=None, end_y=None):
-    encrypted_area = encrypted_image[start_y:end_y, start_x:end_x]
-    cropped_key = generate_key(encrypted_area.shape)
-    decrypted_area = np.zeros_like(encrypted_area)
-    for i in range(3):
-        decrypted_area[:,:,i] = cv2.bitwise_xor(encrypted_area[:,:,i], cropped_key[:,:,i])
-    encrypted_image[start_y:end_y, start_x:end_x] = decrypted_area
+# Encripts the image using the generated Key
+def encrypt(image, key):
+    encrypted_image = cv2.bitwise_xor(image, key)
     return encrypted_image
 
-def main():
-    image = cv2.imread(r'C:\Users\ferra\PycharmProjects\CD1\test\barries.jpg')
+
+# Decrypts the image
+def decrypt(encrypted_image, key):
+    decrypted_image = cv2.bitwise_xor(encrypted_image, key)
+    return decrypted_image
+
+
+# Partially encrypts the image, given two points, start and end
+def encrypt_portion(image, key, start_x, start_y, end_x, end_y):
+    encrypted_image = image.copy()
+    encrypted_image[start_y:end_y, start_x:end_x] = cv2.bitwise_xor(image[start_y:end_y, start_x:end_x],
+                                                                    key[start_y:end_y, start_x:end_x])
+    return encrypted_image
+
+
+# Partially decrypts the image, given two points, start and end
+def decrypt_portion(encrypted_image, key, start_x, start_y, end_x, end_y):
+    decrypted_image = encrypted_image.copy()
+    decrypted_image[start_y:end_y, start_x:end_x] = cv2.bitwise_xor(encrypted_image[start_y:end_y, start_x:end_x],
+                                                                    key[start_y:end_y, start_x:end_x])
+    return decrypted_image
+
+
+def demo_grayscale():
+    image = cv2.imread(r'C:\Users\ferra\PycharmProjects\CD1\teste_pb_2.jpg', cv2.IMREAD_GRAYSCALE)
+
     key = generate_key(image.shape)
-    encrypted_image = encrypt(image, key, start_x=10, start_y=10, end_x=110, end_y=60)
-    decrypted_image = decrypt(encrypted_image, key, start_x=10, start_y=10, end_x=110, end_y=60)
-    cv2.imshow('Original Image', image)
-    cv2.imshow('Encrypted Image', encrypted_image)
-    cv2.imshow('Decrypted Image', decrypted_image)
+
+    encrypted_image = encrypt(image, key)
+
+    decrypted_image = decrypt(encrypted_image, key)
+
+    cv2.imshow('Original Image Whole', image)
+    cv2.imshow('Encrypted Image Whole', encrypted_image)
+    cv2.imshow('Decrypted Image Whole', decrypted_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-if __name__ == "__main__":
-    main()
+    start_x = 100
+    start_y = 200
+    end_x = 400
+    end_y = 500
+
+    encrypted_image_portion = encrypt_portion(image, key, start_x, start_y, end_x, end_y)
+
+    decrypted_image_portion = decrypt_portion(encrypted_image_portion, key, start_x, start_y, end_x, end_y)
+
+    cv2.imshow('Original Image portion', image)
+    cv2.imshow('Encrypted Image portion', encrypted_image_portion)
+    cv2.imshow('Decrypted Image portion', decrypted_image_portion)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def demo_colors():
+    image = cv2.imread(r'C:\Users\ferra\PycharmProjects\CD1\teste_cores_2.jpg', cv2.IMREAD_COLOR)
+    key = generate_key(image.shape)
+    encrypted_image = encrypt(image, key)
+    decrypted_image = decrypt(encrypted_image, key)
+
+    cv2.imshow('Original Image Whole', image)
+    cv2.imshow('Encrypted Image Whole', encrypted_image)
+    cv2.imshow('Decrypted Image Whole', decrypted_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    start_x = 200
+    start_y = 150
+    end_x = 600
+    end_y = 500
+
+    encrypted_image_portion = encrypt_portion(image, key, start_x, start_y, end_x, end_y)
+
+    decrypted_image_portion = decrypt_portion(encrypted_image_portion, key, start_x, start_y, end_x, end_y)
+
+    cv2.imshow('Original Image portion', image)
+    cv2.imshow('Encrypted Image portion', encrypted_image_portion)
+    cv2.imshow('Decrypted Image portion', decrypted_image_portion)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+#demo_colors()
+demo_grayscale()
+
